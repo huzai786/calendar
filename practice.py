b41b5ab69307bf16e86deee47afff1fe5661e919
huzai786 <muhammadhuzaifagamer123@gmail.com>
2022-05-12 16:24:00 +0500

    little tweeks left

--- practice.py
+++ practice.py
@@ -1,11 +1,8 @@
 from datetime import datetime
 from datetime import timedelta
-from packages.temp import get_temperature
 from packages.event_date import CalendarEvent
+from intervaltree import IntervalTree, Interval
 
-
-# date = datetime.now().replace(second=0, minute=0, hour=0, microsecond=0)
-# print(date)
 
 name = 'lehrer'
 path = '/json_files/lehr.json'
@@ -16,6 +13,10 @@ start = datetime(2022, 5, 19, 7, 0)
 end = start + timedelta(hours=3.5)
 x = c.get_event_detail(start, end)
 
-print(x)
+print(start, x, end)
 
+# tree = IntervalTree.from_tuples(x)
+# print(tree)
+# x = tree.remove_overlap(start, end)
 
+# print(tree)
